42770c809507f5634d6bb4cdc6214776dd762f11
Eliu-web <a.23308051281276@cbtis128.edu.mx>
2024-09-24 19:56:31 -0600

    Add files via upload

---
+++ Fahrenheit-celsius.py
@@ -0,0 +1,7 @@
+#En esta actividad, realizaremos un codigo que realiza una conversion de grados farenheit a grados celsius
+
+fahrenheit = int(input("Introduce los grados fahrenheit: ")) #Solicita al usuario introducir un valor
+
+Celsius = (5/9) * (fahrenheit-32) #Indica al programa para realizar la conversion
+
+print ("La conversion es de: ", Celsius) #Muestra el resultado
